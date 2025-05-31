60a62caa865b2a9118ebdc2a26040e80bb6b1df9
GaoNeng-wWw <gaonenggg@outlook.com>
2025-05-31 10:43:19 +0800

    feat: update profile

--- src/profile/dto/update-dto.ts
+++ src/profile/dto/update-dto.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 export const updateProfile = z.object({
   nick: z.string(),
   desc: z.string().optional().nullable(),
+  avatar: z.string()
 });
 
 export class UpdateProfile extends createZodDto(updateProfile.partial()) {}
